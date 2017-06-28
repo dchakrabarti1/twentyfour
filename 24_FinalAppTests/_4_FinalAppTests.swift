f830520995a85e25d64ad3889ea05322f4c06019
dchakrabarti1 <Devanshi.Chakrabarti@gmail.com>
2017-06-28 12:35:37 -0500

    Initial Commit

---
+++ 24_FinalAppTests/_4_FinalAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  _4_FinalAppTests.swift
+//  24_FinalAppTests
+//
+//  Created by Devanshi Chakrabarti on 6/28/17.
+//  Copyright © 2017 Devanshi Chakrabarti. All rights reserved.
+//
+
+import XCTest
+@testable import _4_FinalApp
+
+class _4_FinalAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
